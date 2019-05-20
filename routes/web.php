6f9ae48cd249fd9c29aa04800eaf15bfd56e1fd5
Hiran Nuwanpriya <hiran@gdsmc.be>
2019-05-20 16:18:13 +0530

    init commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/curriculum-vitae', 'CurriculumVitaeController@index')->name('curriculum-vitae');
+
+Route::get('/curriculum-vitae/add', 'CurriculumVitaeController@create')->name('curriculum-vitae-add');
+
+Route::get('/curriculum-vitae/edit', 'CurriculumVitaeController@edit')->name('curriculum-vitae-edit');
+
+
+Route::get('/admin/login', [
+    'as' => 'admin.login-form',
+    'uses' => 'Auth\AdminLoginController@showLoginForm'
+]);
+
+Route::post('/admin/login', [
+    'as' => 'admin.login',
+    'uses' => 'Auth\AdminLoginController@login'
+]);
+
+
+Route::get('/admin', [
+    'as' => 'administration',
+//    'middleware' => ['auth:admin'],
+    'middleware' => ['admin:super_admin', 'auth:admin'],
+    'uses' => function () {
+        return 'ela';
+    }
+]);
